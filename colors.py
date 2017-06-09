4fd18be8354a1da6aa0d2ac3e382b3e2bd2a2027
Ann Kidder <annkidder@Anns-Air-2.home>
2017-06-09 09:08:35 -0400

    adding colors module

---
+++ colors.py
@@ -0,0 +1,9 @@
+class bcolors:
+    HEADER = '\033[95m\033[1m'
+    OKBLUE = '\033[94m\033[1m'
+    OKGREEN = '\033[92m\033[1m'
+    WARNING = '\033[93m\033[1m'
+    FAIL = '\033[91m\033[1m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
